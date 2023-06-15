5ad00c76227b74771bd365850fdd6be0a1c57c6e
d-Fre <jumafredrick86@gmail.com>
2023-06-15 16:47:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main -Entry point
  *
- * Return: Always O (Success)
+ * Return: Always O ( Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (O);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (O);
 }
