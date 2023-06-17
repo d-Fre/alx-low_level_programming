8a0b319799337bea9f1fc7450d0772f9cae46263
d-Fre <jumafredrick86@gmail.com>
2023-06-16 17:27:58 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char k;
+	
+	k = 'a';
+	while
+		(k <= 'z') {
+			putchar(k);
+			k++;
+		}
+	putchar('\n');
+	return (0);
+}
