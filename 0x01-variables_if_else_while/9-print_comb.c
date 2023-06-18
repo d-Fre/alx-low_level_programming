c9563b2a14ca944f28be20f6d54cd82d61d0d67b
d-Fre <jumafredrick86@gmail.com>
2023-06-17 18:31:55 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,14 +6,17 @@
 
 int main(void)
 {
-	int n;
+	int n = '0';
 
-	for (n = 48; n < 58; n++)
-		putchar(n);
-	if (n != 57)
+	while (n <= '9')
 	{
-		putchar(',');
-		putchar(' ');
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		++n;
 	}
 	putchar('\n');
 	return (0);
